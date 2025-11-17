b637a21ccf127a2d137f10b2e125cd814152f408
cgxspartans <cgxspartans@proton.me>
2025-11-17 07:12:55 -0500

    Add main Spring Boot application class for project scaffolding.

---
+++ SpringBootProject/src/main/java/com/warehouseems/WarehouseEmsApplication.java
@@ -0,0 +1,11 @@
+package com.warehouseems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseEmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseEmsApplication.class, args);
+    }
+}
