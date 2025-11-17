4038d5e4ec49a8f370b045e48bac0d64e9a9d310
cgxspartans <cgxspartans@proton.me>
2025-11-17 01:56:51 -0500

    Initial commit: Added main Spring Boot application class for Warehouse EMS.

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/WarehouseEmsApplication.java
@@ -0,0 +1,17 @@
+package com.warehouse.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the Warehouse EMS Spring Boot application.
+ *
+ * @author Spartan-TA
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class WarehouseEmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseEmsApplication.class, args);
+    }
+}
