f2d26f635e84899e6d8423d61c4e91f348a61db0
cgxspartans <cgxspartans@proton.me>
2025-11-19 08:43:26 -0500

    Add AssetService for Equipment & Asset Assignment (E09)

---
+++ SpringBootProject/src/main/java/com/example/asset/AssetService.java
@@ -0,0 +1,29 @@
+package com.example.asset;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AssetService {
+    @Autowired
+    private AssetRepository assetRepository;
+
+    public List<Asset> getAllAssets() {
+        return assetRepository.findAll();
+    }
+
+    public Optional<Asset> getAssetById(Long id) {
+        return assetRepository.findById(id);
+    }
+
+    public Asset saveAsset(Asset asset) {
+        return assetRepository.save(asset);
+    }
+
+    public void deleteAsset(Long id) {
+        assetRepository.deleteById(id);
+    }
+}
