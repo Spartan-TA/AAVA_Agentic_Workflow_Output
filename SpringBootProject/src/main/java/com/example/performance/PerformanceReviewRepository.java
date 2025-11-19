1c17a8483fd5daa984bce8b1867b7aadd8ff302a
Christina Torres <38227182+cchloet@users.noreply.github.com>
2025-11-19 08:56:20 -0500

    Delete SpringBootProject directory

--- SpringBootProject/src/main/java/com/example/performance/PerformanceReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.performance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PerformanceReviewRepository extends JpaRepository<PerformanceReview, Long> {
-    // Custom query methods can be added here
-}
--- SpringBootProject/src/main/java/com/example/safety/SafetyIncidentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.safety;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SafetyIncidentRepository extends JpaRepository<SafetyIncident, Long> {
-    // Custom query methods can be added here
-}
