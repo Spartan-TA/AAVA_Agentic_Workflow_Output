9b969a8ac4e5dcc098564c30498cdff9efa8fafb
cgxspartans <cgxspartans@proton.me>
2025-12-01 01:27:27 -0500

    Add SftpConfig for SFTP session factory.

---
+++ SpringBootProject/config/SftpConfig.java
@@ -0,0 +1,33 @@
+package com.example.ems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class SftpConfig {
+
+    @Value("${sftp.host}")
+    private String sftpHost;
+
+    @Value("${sftp.port}")
+    private int sftpPort;
+
+    @Value("${sftp.user}")
+    private String sftpUser;
+
+    @Value("${sftp.password}")
+    private String sftpPassword;
+
+    @Bean
+    public DefaultSftpSessionFactory sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost(sftpHost);
+        factory.setPort(sftpPort);
+        factory.setUser(sftpUser);
+        factory.setPassword(sftpPassword);
+        factory.setAllowUnknownKeys(true);
+        return factory;
+    }
+}
