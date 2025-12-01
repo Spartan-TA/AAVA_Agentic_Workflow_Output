fed1c1a2b345be2d1354239f7942f42e53decdcb
cgxspartans <cgxspartans@proton.me>
2025-12-01 00:56:14 -0500

    Add EmployeeRepository interface extending JpaRepository for Employee entity.

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/employee/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.warehouse.ems.employee.repository;
+
+import com.warehouse.ems.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository for Employee entity.
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+    Page<Employee> findAllByDeletedFalse(Pageable pageable);
+    Optional<Employee> findByIdAndDeletedFalse(Long id);
+    boolean existsByBadgeIdAndDeletedFalse(String badgeId);
+}
