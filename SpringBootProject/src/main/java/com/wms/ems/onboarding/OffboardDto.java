de04287d38d6551fa7a4d1c5e2f43e54ac4745f6
cgxspartans <cgxspartans@proton.me>
2025-12-02 02:56:01 -0500

    Add OffboardDto for offboarding API (E17)

---
+++ SpringBootProject/src/main/java/com/wms/ems/onboarding/OffboardDto.java
@@ -0,0 +1,10 @@
+package com.wms.ems.onboarding;
+
+import lombok.Data;
+
+@Data
+public class OffboardDto {
+    private Long employeeId;
+    private String reason;
+    // Additional fields as needed
+}
