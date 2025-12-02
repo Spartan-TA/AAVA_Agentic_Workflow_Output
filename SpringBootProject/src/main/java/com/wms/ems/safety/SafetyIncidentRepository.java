48ac0e8bbbba40834a2ffd40b6f5a02d4e030dcd
vikram <vervik631@gmail.com>
2025-12-02 13:21:16 +0530

    Delete SpringBootProject directory

--- SpringBootProject/src/main/java/com/wms/ems/safety/SafetyIncidentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wms.ems.safety;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SafetyIncidentRepository extends JpaRepository<SafetyIncident, Long> {
-}
