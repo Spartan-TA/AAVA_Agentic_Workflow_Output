6d0000ee6a0748a65b34fe11e2505cc4346cd0f0
cgxspartans <cgxspartans@proton.me>
2025-12-08 13:55:28 -0500

    Add TrainingStatus enum for training module.

---
+++ SpringBootProject/src/main/java/com/warehouse/enums/TrainingStatus.java
@@ -0,0 +1,11 @@
+package com.warehouse.enums;
+
+/**
+ * Enum representing status of training sessions.
+ */
+public enum TrainingStatus {
+    SCHEDULED,
+    COMPLETED,
+    CANCELLED,
+    IN_PROGRESS
+}
