c87827369a1f254d32862e09ea73b3b3a09fe362
cgxspartans <cgxspartans@proton.me>
2025-12-08 13:55:40 -0500

    Add TaskStatus enum for task module.

---
+++ SpringBootProject/src/main/java/com/warehouse/enums/TaskStatus.java
@@ -0,0 +1,12 @@
+package com.warehouse.enums;
+
+/**
+ * Enum representing status of tasks.
+ */
+public enum TaskStatus {
+    OPEN,
+    IN_PROGRESS,
+    COMPLETED,
+    BLOCKED,
+    CANCELLED
+}
