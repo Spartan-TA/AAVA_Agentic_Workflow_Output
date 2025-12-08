8f060b7caa20f98c71a38f415eae8fe36c3f9034
cgxspartans <cgxspartans@proton.me>
2025-12-08 13:55:31 -0500

    Add IncidentStatus enum for incident reports.

---
+++ SpringBootProject/src/main/java/com/warehouse/enums/IncidentStatus.java
@@ -0,0 +1,11 @@
+package com.warehouse.enums;
+
+/**
+ * Enum representing status of incident reports.
+ */
+public enum IncidentStatus {
+    REPORTED,
+    INVESTIGATING,
+    RESOLVED,
+    CLOSED
+}
