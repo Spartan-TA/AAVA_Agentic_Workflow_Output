76295cbf031804a97a42685a91584452fb42eea8
cgxspartans <cgxspartans@proton.me>
2025-12-08 13:55:38 -0500

    Add TransactionType enum for inventory transactions.

---
+++ SpringBootProject/src/main/java/com/warehouse/enums/TransactionType.java
@@ -0,0 +1,11 @@
+package com.warehouse.enums;
+
+/**
+ * Enum representing types of inventory transactions.
+ */
+public enum TransactionType {
+    INBOUND,
+    OUTBOUND,
+    ADJUSTMENT,
+    TRANSFER
+}
