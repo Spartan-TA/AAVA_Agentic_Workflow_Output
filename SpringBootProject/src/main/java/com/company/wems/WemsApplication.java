4b6d60363383a390ec51ce9922a01b7fdcc8ae13
cgxspartans <cgxspartans@proton.me>
2025-12-09 05:11:50 -0500

    Add main Spring Boot application class.

---
+++ SpringBootProject/src/main/java/com/company/wems/WemsApplication.java
@@ -0,0 +1,14 @@
+package com.company.wems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for Warehouse Employee Management System.
+ */
+@SpringBootApplication
+public class WemsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WemsApplication.class, args);
+    }
+}
