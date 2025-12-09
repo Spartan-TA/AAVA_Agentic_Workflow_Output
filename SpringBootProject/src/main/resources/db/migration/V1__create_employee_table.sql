aebe8c49eb7de167672b1a17959875e91beb1be5
vikram <vervik631@gmail.com>
2025-12-09 14:51:36 +0530

    Delete SpringBootProject directory

--- SpringBootProject/src/main/resources/db/migration/V1__create_employee_table.sql
+++
@@ -1,9 +0,0 @@
--- Flyway migration script to create employees table
-CREATE TABLE employees (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    role VARCHAR(100) NOT NULL,
-    hire_date DATE NOT NULL
-);
