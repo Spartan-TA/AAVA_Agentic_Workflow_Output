fa5520621bb0d3bc4a3fe9194510db123d129604
cgxspartans <cgxspartans@proton.me>
2025-12-15 01:20:58 -0500

    Add AttendanceEventType enum for Warehouse EMS

---
+++ SpringBootProject/src/main/java/com/warehouseems/attendance/entity/AttendanceEventType.java
@@ -0,0 +1,11 @@
+package com.warehouseems.attendance.entity;
+
+/**
+ * Enum for attendance event types.
+ */
+public enum AttendanceEventType {
+    CLOCK_IN,
+    CLOCK_OUT,
+    BREAK_START,
+    BREAK_END
+}
