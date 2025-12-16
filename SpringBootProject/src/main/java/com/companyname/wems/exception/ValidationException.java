1ebbbc144f21e565d738f05ac31c47c2daba4c3f
missingStudent <cchloet2.0@gmail.com>
2025-12-16 13:46:13 -0500

    Removed files

--- SpringBootProject/src/main/java/com/companyname/wems/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.companyname.wems.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
--- SpringBootProject/src/main/resources/db/migration/V2__Insert_Initial_Data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO roles (name) VALUES ('ADMIN'), ('HR'), ('SUPERVISOR'), ('WORKER');
-
-INSERT INTO users (username, password) VALUES ('admin', '$2a$10$7QJwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw'); -- bcrypt hash for 'admin123'
-
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1); -- Assign ADMIN role to admin user
