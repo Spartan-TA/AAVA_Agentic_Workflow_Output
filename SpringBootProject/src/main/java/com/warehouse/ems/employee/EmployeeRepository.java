43f0d38023d53de824d98a3f6f7e59ecac164961
cgxspartans <cgxspartans@proton.me>
2025-12-18 04:14:00 -0500

    Add EmployeeRepository for data access.

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/employee/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.warehouse.ems.employee;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeId(String badgeId);
+    Page<Employee> findAllByDeletedFalse(Pageable pageable);
+}
