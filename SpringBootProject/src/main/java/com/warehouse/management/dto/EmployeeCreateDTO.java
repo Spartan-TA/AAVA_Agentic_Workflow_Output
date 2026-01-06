12af2cde7ab9b3d906ae10f4ea6cb6ac9a3d86d1
cgxspartans <cgxspartans@proton.me>
2026-01-06 14:37:56 -0500

    Add EmployeeCreateDTO with validation annotations

---
+++ SpringBootProject/src/main/java/com/warehouse/management/dto/EmployeeCreateDTO.java
@@ -0,0 +1,35 @@
+package com.warehouse.management.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeeCreateDTO {
+    @NotBlank
+    @Size(max = 100)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 100)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    @Size(max = 150)
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^\+?[0-9]{7,15}$", message = "Invalid phone number")
+    private String phoneNumber;
+
+    @NotBlank
+    @Size(max = 100)
+    private String jobTitle;
+
+    @NotBlank
+    @Size(max = 100)
+    private String department;
+}
