40bd47177eddbf16b3f27b29283e2c26cbfed7b5
cgxspartans <cgxspartans@proton.me>
2026-01-06 14:18:32 -0500

    Add Status enum for entity states

---
+++ SpringBootProject/src/main/java/com/companyname/warehouse/common/enums/Status.java
@@ -0,0 +1,11 @@
+package com.companyname.warehouse.common.enums;
+
+/**
+ * Enum representing common entity status.
+ */
+public enum Status {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    DELETED
+}
