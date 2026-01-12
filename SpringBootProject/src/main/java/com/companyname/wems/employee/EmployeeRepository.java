e296b987954a70f9050124368dded4f561f65bc0
cgxspartans <cgxspartans@proton.me>
2026-01-12 10:52:02 -0500

    Add EmployeeRepository interface for Employee CRUD and custom queries

---
+++ SpringBootProject/src/main/java/com/companyname/wems/employee/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.companyname.wems.employee;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeId(String badgeId);
+    @Query("SELECT e FROM Employee e WHERE e.deleted = false")
+    Page<Employee> findAllActive(Pageable pageable);
+}
