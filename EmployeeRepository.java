e4aaf69f49a79cc9fbfb920f633cc06a710e743a
cgxspartans <cgxspartans@proton.me>
2026-01-12 01:33:09 -0500

    Add EmployeeRepository.java - Employee JPA repository interface

---
+++ EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.warehouseems.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeId(String badgeId);
+    List<Employee> findByStatus(String status);
+}
