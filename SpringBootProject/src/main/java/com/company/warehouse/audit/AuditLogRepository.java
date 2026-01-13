98e3c1d1b4b651114b9755b47da2243d62fa440e
vikram <vervik631@gmail.com>
2026-01-13 14:12:58 +0530

    Delete SpringBootProject directory

--- SpringBootProject/src/main/java/com/company/warehouse/audit/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-// src/main/java/com/company/warehouse/audit/AuditLogRepository.java
-package com.company.warehouse.audit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
