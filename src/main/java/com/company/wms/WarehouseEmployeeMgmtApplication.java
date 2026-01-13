c2b217fe3d3980bf44d1249345556de8c69c9f92
cgxspartans <cgxspartans@proton.me>
2026-01-13 10:06:31 -0500

    Add main Spring Boot application class for Warehouse Employee Management System.

---
+++ src/main/java/com/company/wms/WarehouseEmployeeMgmtApplication.java
@@ -0,0 +1,11 @@
+package com.company.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseEmployeeMgmtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseEmployeeMgmtApplication.class, args);
+    }
+}
