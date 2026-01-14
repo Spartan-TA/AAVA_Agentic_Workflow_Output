53b410680998e1fcd8f1127e130ffc462356793b
cgxspartans <cgxspartans@proton.me>
2026-01-14 08:57:05 -0500

    Add Department entity class

---
+++ core/src/main/java/com/warehouse/core/entity/Department.java
@@ -0,0 +1,23 @@
+package com.warehouse.core.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+}
