e824082c5c01b0231760d88873e07a04384255d9
cgxspartans <cgxspartans@proton.me>
2026-01-14 00:45:30 -0500

    Add HRISIntegrationService for HRIS integration logic

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/integration/HRISIntegrationService.java
@@ -0,0 +1,10 @@
+package com.warehouse.ems.integration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HRISIntegrationService {
+    public void syncEmployees() {
+        // Logic to sync employees with HRIS
+    }
+}
