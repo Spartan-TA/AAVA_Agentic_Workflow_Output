65213a321a1177d2b6140dc1c208854423c78153
cgxspartans <cgxspartans@proton.me>
2026-01-14 00:45:33 -0500

    Add WMSIntegrationService for WMS integration logic

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/integration/WMSIntegrationService.java
@@ -0,0 +1,10 @@
+package com.warehouse.ems.integration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WMSIntegrationService {
+    public void syncInventory() {
+        // Logic to sync inventory with WMS
+    }
+}
