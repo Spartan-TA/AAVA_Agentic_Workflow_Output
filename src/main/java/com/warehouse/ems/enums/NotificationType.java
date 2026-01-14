9c0c407789dcf4a343007a82ba335c22981963d8
cgxspartans <cgxspartans@proton.me>
2026-01-14 00:51:50 -0500

    Add NotificationType enum for notification types

---
+++ src/main/java/com/warehouse/ems/enums/NotificationType.java
@@ -0,0 +1,11 @@
+package com.warehouse.ems.enums;
+
+/**
+ * Enum representing types of notifications.
+ */
+public enum NotificationType {
+    SHIFT_CHANGE,
+    CERT_EXPIRY,
+    APPROVAL,
+    ANNOUNCEMENT
+}
