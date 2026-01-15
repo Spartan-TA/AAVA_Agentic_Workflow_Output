e6e36093eb673bd4c92dafe1973bd444472cef65
missingStudent <cchloet2.0@gmail.com>
2026-01-15 10:46:21 -0500

    Removed morning demo files

--- SpringBootProject/src/main/java/com/warehouse/ems/audit/Auditable.java
+++
@@ -1,9 +0,0 @@
-package com.warehouse.ems.audit;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Auditable {
-    String action();
-}
