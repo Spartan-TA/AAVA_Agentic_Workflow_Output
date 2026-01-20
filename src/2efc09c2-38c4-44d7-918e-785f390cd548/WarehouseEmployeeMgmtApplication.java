b1afa24ab8b76c4da0b0afcf47092ffdadb10e62
cgxspartans <cgxspartans@proton.me>
2026-01-20 14:26:35 -0500

    Add WarehouseEmployeeMgmtApplication.java as the main Spring Boot entry point

---
+++ src/2efc09c2-38c4-44d7-918e-785f390cd548/WarehouseEmployeeMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.company.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WarehouseEmployeeMgmtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseEmployeeMgmtApplication.class, args);
+    }
+}
