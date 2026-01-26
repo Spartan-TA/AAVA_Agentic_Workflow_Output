a774362b88313e0f242e2aa4be3aa909c2cc1b60
vikram <vervik631@gmail.com>
2026-01-26 14:10:14 +0530

    Delete SpringBootProject directory

--- SpringBootProject/src/main/resources/db/migration/V2__Insert_reference_data.sql
+++
@@ -1,7 +0,0 @@
--- V2: Insert reference data for roles and departments
-INSERT INTO department (name) VALUES ('Operations'), ('Logistics'), ('HR'), ('IT'), ('Maintenance');
-
--- Roles are typically handled in the application, but for demo purposes:
--- If you have a roles table, insert roles. Otherwise, roles are in code.
--- Example:
--- INSERT INTO role (name) VALUES ('ADMIN'), ('HR'), ('SUPERVISOR'), ('WORKER');
