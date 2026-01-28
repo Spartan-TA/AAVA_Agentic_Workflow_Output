009678ac4a4a50929f9d8440f8a280ba222cbe32
cgxspartans <cgxspartans@proton.me>
2026-01-28 09:46:14 -0500

    Add main Spring Boot application entry point

---
+++ SpringBootProject/src/main/java/com/warehouse/management/WarehouseManagementApplication.java
@@ -0,0 +1,14 @@
+package com.warehouse.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for Warehouse Employee Management System.
+ */
+@SpringBootApplication
+public class WarehouseManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseManagementApplication.class, args);
+    }
+}
