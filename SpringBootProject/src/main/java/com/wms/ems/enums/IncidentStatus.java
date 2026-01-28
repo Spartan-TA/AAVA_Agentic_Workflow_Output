a438a3bd014b6dbfec83bcef94155cb070931fa3
cgxspartans <cgxspartans@proton.me>
2026-01-28 00:55:48 -0500

    Add IncidentStatus enum for safety incident workflow

---
+++ SpringBootProject/src/main/java/com/wms/ems/enums/IncidentStatus.java
@@ -0,0 +1,13 @@
+package com.wms.ems.enums;
+
+/**
+ * Enum representing the status of safety incidents.
+ * OPEN: Incident has been reported and is open.
+ * INVESTIGATING: Incident is under investigation.
+ * RESOLVED: Incident has been resolved with corrective actions.
+ */
+public enum IncidentStatus {
+    OPEN,
+    INVESTIGATING,
+    RESOLVED
+}
