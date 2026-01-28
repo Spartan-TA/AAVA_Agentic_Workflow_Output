4eafb4416c47c6df8aac69ca1567f2540e764d02
cgxspartans <cgxspartans@proton.me>
2026-01-28 09:46:29 -0500

    Add cache configuration to enable Spring caching

---
+++ SpringBootProject/src/main/java/com/warehouse/management/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package com.warehouse.management.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Enables caching for the application.
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
