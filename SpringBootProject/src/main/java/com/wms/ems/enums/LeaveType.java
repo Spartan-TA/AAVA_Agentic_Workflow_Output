8a59fcd33eac9259cdbd5f4acf8bcddfe0e64afe
cgxspartans <cgxspartans@proton.me>
2026-01-28 00:55:38 -0500

    Add LeaveType enum for PTO, sick, and unpaid leave

---
+++ SpringBootProject/src/main/java/com/wms/ems/enums/LeaveType.java
@@ -0,0 +1,13 @@
+package com.wms.ems.enums;
+
+/**
+ * Enum representing types of leave requests.
+ * PTO: Paid Time Off.
+ * SICK: Sick leave.
+ * UNPAID: Unpaid leave.
+ */
+public enum LeaveType {
+    PTO,
+    SICK,
+    UNPAID
+}
