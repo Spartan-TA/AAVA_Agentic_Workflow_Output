fbb184d4e9dbc079cb88662973c058568ead7439
cgxspartans <cgxspartans@proton.me>
2026-01-29 11:50:44 -0500

    Add EmployeeService class

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/management/employee/service/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.warehouse.employee.management.employee.service;
+
+import com.warehouse.employee.management.employee.domain.Employee;
+import com.warehouse.employee.management.employee.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepo.findById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteById(id);
+    }
+}
