7d462530a3bf0404b63327bbe75df13e2c37a7cb
cgxspartans <cgxspartans@proton.me>
2026-01-29 10:37:46 -0500

    Add EmployeeRepository.java for employee JPA repository

---
+++ SpringBootProject/src/main/java/com/company/wms/employee/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.company.wms.employee.repository;
+
+import com.company.wms.employee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository for Employee entity.
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeId(String badgeId);
+    Optional<Employee> findByEmail(String email);
+}
