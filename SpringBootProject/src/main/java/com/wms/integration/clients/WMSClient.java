5cdaa0dda9e7f62088704a353906fa059ce90766
cgxspartans <cgxspartans@proton.me>
2026-02-02 09:12:24 -0500

    Add WMSClient for WMS integration (EPIC 13)

---
+++ SpringBootProject/src/main/java/com/wms/integration/clients/WMSClient.java
@@ -0,0 +1,11 @@
+package com.wms.integration.clients;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Client for integrating with Warehouse Management System (WMS) APIs.
+ */
+@Component
+public class WMSClient {
+    // TODO: Implement methods for WMS integration (fetch inventory, update status, etc.)
+}
