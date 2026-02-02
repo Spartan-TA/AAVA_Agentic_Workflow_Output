2a4b0f72237ada0b99e307b9722d1480e9a48a07
cgxspartans <cgxspartans@proton.me>
2026-02-02 08:54:19 -0500

    Add LeaveStatus enum for leave request status management

---
+++ SpringBootProject/src/main/java/com/wms/leave/domain/LeaveStatus.java
@@ -0,0 +1,11 @@
+package com.wms.leave.domain;
+
+/**
+ * Enum representing the status of a leave request.
+ */
+public enum LeaveStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELLED
+}
