e6642f25665eec41a71e8ebef8a595a93ce1cdd8
cgxspartans <cgxspartans@proton.me>
2026-02-02 08:57:26 -0500

    Add Role enum with ADMIN, HR, SUPERVISOR, WORKER values

---
+++ SpringBootProject/src/main/java/com/wms/common/enums/Role.java
@@ -0,0 +1,11 @@
+package com.wms.common.enums;
+
+/**
+ * Enum representing user roles in the Warehouse Employee Management System.
+ */
+public enum Role {
+    ADMIN,
+    HR,
+    SUPERVISOR,
+    WORKER
+}
