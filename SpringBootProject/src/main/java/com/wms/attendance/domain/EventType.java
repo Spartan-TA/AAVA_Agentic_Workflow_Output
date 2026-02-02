7119df7834b22bd3fe3f87da381ceaedd8f5a26a
cgxspartans <cgxspartans@proton.me>
2026-02-02 08:54:11 -0500

    Add EventType enum for attendance event types

---
+++ SpringBootProject/src/main/java/com/wms/attendance/domain/EventType.java
@@ -0,0 +1,13 @@
+package com.wms.attendance.domain;
+
+/**
+ * Enum representing the types of attendance events.
+ */
+public enum EventType {
+    CLOCK_IN,
+    CLOCK_OUT,
+    BREAK_START,
+    BREAK_END,
+    OVERTIME_START,
+    OVERTIME_END
+}
