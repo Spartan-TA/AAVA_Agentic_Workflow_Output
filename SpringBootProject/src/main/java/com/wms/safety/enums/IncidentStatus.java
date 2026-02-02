ec2c7a9014661c8f077c1a3dc2f3b3e92a89bc50
cgxspartans <cgxspartans@proton.me>
2026-02-02 09:07:15 -0500

    Add IncidentStatus enum for Safety Incidents & OSHA Reporting (EPIC 8)

---
+++ SpringBootProject/src/main/java/com/wms/safety/enums/IncidentStatus.java
@@ -0,0 +1,11 @@
+package com.wms.safety.enums;
+
+/**
+ * Enum representing status of a safety incident
+ */
+public enum IncidentStatus {
+    OPEN,
+    IN_PROGRESS,
+    CLOSED,
+    REPORTED_TO_OSHA
+}
