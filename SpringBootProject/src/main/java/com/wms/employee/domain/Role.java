8488cb33345172a15d60a92d63a5a1e595e44632
cgxspartans <cgxspartans@proton.me>
2026-02-02 08:54:07 -0500

    Add Role enum for employee role management

---
+++ SpringBootProject/src/main/java/com/wms/employee/domain/Role.java
@@ -0,0 +1,12 @@
+package com.wms.employee.domain;
+
+/**
+ * Enum representing the roles available in the system for RBAC.
+ */
+public enum Role {
+    ADMIN,
+    SUPERVISOR,
+    MANAGER,
+    EMPLOYEE,
+    HR
+}
