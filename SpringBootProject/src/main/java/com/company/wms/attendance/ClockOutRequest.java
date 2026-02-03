b8c06d82f04507fee593e826072cdc5916edb357
cgxspartans <cgxspartans@proton.me>
2026-02-03 15:15:09 -0500

    Add ClockOutRequest DTO for clock-out API endpoint

---
+++ SpringBootProject/src/main/java/com/company/wms/attendance/ClockOutRequest.java
@@ -0,0 +1,37 @@
+package com.company.wms.attendance;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * DTO for clock-out requests.
+ */
+public class ClockOutRequest {
+
+    @NotNull
+    private Long employeeId;
+
+    @NotNull
+    @Size(max = 64)
+    private String deviceId;
+
+    @NotNull
+    private Double latitude;
+
+    @NotNull
+    private Double longitude;
+
+    public ClockOutRequest() {}
+
+    public Long getEmployeeId() { return employeeId; }
+    public void setEmployeeId(Long employeeId) { this.employeeId = employeeId; }
+
+    public String getDeviceId() { return deviceId; }
+    public void setDeviceId(String deviceId) { this.deviceId = deviceId; }
+
+    public Double getLatitude() { return latitude; }
+    public void setLatitude(Double latitude) { this.latitude = latitude; }
+
+    public Double getLongitude() { return longitude; }
+    public void setLongitude(Double longitude) { this.longitude = longitude; }
+}
