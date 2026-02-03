c1c0dffbb3dfe8d95fddf726a61410e4a498b18c
cgxspartans <cgxspartans@proton.me>
2026-02-03 15:21:23 -0500

    Add LeaveType enum for Leave module

---
+++ SpringBootProject/src/main/java/com/company/wms/leave/LeaveType.java
@@ -0,0 +1,16 @@
+package com.company.wms.leave;
+
+/**
+ * Enum representing types of leave.
+ */
+public enum LeaveType {
+    SICK,
+    VACATION,
+    PERSONAL,
+    UNPAID,
+    MATERNITY,
+    PATERNITY,
+    BEREAVEMENT,
+    JURY_DUTY,
+    OTHER
+}
