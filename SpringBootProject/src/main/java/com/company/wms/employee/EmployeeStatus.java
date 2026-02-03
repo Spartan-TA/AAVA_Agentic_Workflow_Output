19026c7c6941cdbfe266e500cbd2e33a4f49df28
cgxspartans <cgxspartans@proton.me>
2026-02-03 15:12:25 -0500

    Add EmployeeStatus.java enum

---
+++ SpringBootProject/src/main/java/com/company/wms/employee/EmployeeStatus.java
@@ -0,0 +1,12 @@
+package com.company.wms.employee;
+
+/**
+ * Enum for employee status.
+ */
+public enum EmployeeStatus {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    TERMINATED,
+    ON_LEAVE
+}
