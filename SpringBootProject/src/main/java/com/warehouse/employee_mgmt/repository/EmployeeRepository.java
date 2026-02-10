89e09aa44f0409a2c144210be4b320794a187009
cgxspartans <cgxspartans@proton.me>
2026-02-10 14:02:21 -0500

    Add EmployeeRepository for Employee CRUD operations (E02).

---
+++ SpringBootProject/src/main/java/com/warehouse/employee_mgmt/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.warehouse.employee_mgmt.repository;
+
+import com.warehouse.employee_mgmt.domain.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+    Page<Employee> findAllByDeletedFalse(Pageable pageable);
+}
