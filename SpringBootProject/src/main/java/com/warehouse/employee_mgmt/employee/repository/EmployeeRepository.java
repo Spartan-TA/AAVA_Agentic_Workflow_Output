c1a4bcb3ad54562a8025afad88138ef4bcc36492
cgxspartans <cgxspartans@proton.me>
2026-02-17 14:10:43 -0500

    Add Employee repository with Spring Data JPA

---
+++ SpringBootProject/src/main/java/com/warehouse/employee_mgmt/employee/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.warehouse.employee_mgmt.employee.repository;
+
+import com.warehouse.employee_mgmt.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+    Page<Employee> findAllByDeletedFalse(Pageable pageable);
+    // Add filtering methods as needed
+}
