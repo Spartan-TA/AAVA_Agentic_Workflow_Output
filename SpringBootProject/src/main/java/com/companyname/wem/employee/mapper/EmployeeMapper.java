841aa1313e43fdbfb3106c35f161630992ca896b
cgxspartans <cgxspartans@proton.me>
2026-02-18 09:27:51 -0500

    Add EmployeeMapper interface using MapStruct

---
+++ SpringBootProject/src/main/java/com/companyname/wem/employee/mapper/EmployeeMapper.java
@@ -0,0 +1,15 @@
+package com.companyname.wem.employee.mapper;
+
+import com.companyname.wem.employee.domain.Employee;
+import com.companyname.wem.employee.dto.EmployeeDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
+
+    EmployeeDTO toDto(Employee employee);
+    Employee toEntity(EmployeeDTO dto);
+}
