5838ccef9e171654bdfa1f89f36e1f6dbd54d607
cgxspartans <cgxspartans@proton.me>
2026-02-18 09:38:57 -0500

    Add main Spring Boot application class

---
+++ EmployeeApplication.java
@@ -0,0 +1,13 @@
+// Intended path: src/main/java/com/companyname/wem/EmployeeApplication.java
+
+package com.companyname.wem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeApplication.class, args);
+    }
+}
