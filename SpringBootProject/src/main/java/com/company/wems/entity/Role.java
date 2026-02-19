29d4032bbf5739c9611f21f64e5075ae2a16edf6
cgxspartans <cgxspartans@proton.me>
2026-02-19 11:26:12 -0500

    Add Role entity for RBAC and security

---
+++ SpringBootProject/src/main/java/com/company/wems/entity/Role.java
@@ -0,0 +1,23 @@
+package com.company.wems.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+/**
+ * Role entity for RBAC.
+ */
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
