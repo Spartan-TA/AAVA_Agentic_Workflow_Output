56a91d61f5a20c7d1634d70c136b2f945700cf78
cgxspartans <cgxspartans@proton.me>
2026-02-26 11:24:33 -0500

    Add NotificationRepository interface

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.employee.repository;
+
+import com.warehouse.employee.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
