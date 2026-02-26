4dddd6fd059207d22fa67e199fb75e6e58fdf9eb
cgxspartans <cgxspartans@proton.me>
2026-02-26 11:22:50 -0500

    Add RoleRepository interface

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.employee.repository;
+
+import com.warehouse.employee.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
