8d25db822b03f97ec8f0619b0671595ea5ed9f2b
cgxspartans <cgxspartans@proton.me>
2026-02-26 11:23:26 -0500

    Add AuditLogRepository interface

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.employee.repository;
+
+import com.warehouse.employee.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
