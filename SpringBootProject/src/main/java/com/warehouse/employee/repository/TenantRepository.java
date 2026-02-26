8b9fbb09e453362192ffbe92bb47c95c486693a0
cgxspartans <cgxspartans@proton.me>
2026-02-26 11:24:51 -0500

    Add TenantRepository interface

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/repository/TenantRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.employee.repository;
+
+import com.warehouse.employee.entity.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+}
