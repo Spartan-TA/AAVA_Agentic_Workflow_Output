149a23a5f8958c963295515e3d08ed1c03078587
cgxspartans <cgxspartans@proton.me>
2026-02-26 11:22:44 -0500

    Add EmployeeRepository interface

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.employee.repository;
+
+import com.warehouse.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
