39f5a1658ce1ea929386044765c704d1a968c3ea
cgxspartans <cgxspartans@proton.me>
2026-03-02 09:33:56 -0500

    Add EmployeeCertificationRepository for Certification module

---
+++ SpringBootProject/src/main/java/com/wems/certification/domain/EmployeeCertificationRepository.java
@@ -0,0 +1,6 @@
+package com.wems.certification.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeCertificationRepository extends JpaRepository<EmployeeCertification, Long> {
+}
