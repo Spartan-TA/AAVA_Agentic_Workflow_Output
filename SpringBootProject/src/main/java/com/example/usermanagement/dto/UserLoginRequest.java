4a4b3767ba5a4351310ea90130d0d79adb7d1e3a
cgxspartans <cgxspartans@proton.me>
2026-03-02 00:48:40 -0500

    Add UserLoginRequest DTO

---
+++ SpringBootProject/src/main/java/com/example/usermanagement/dto/UserLoginRequest.java
@@ -0,0 +1,13 @@
+package com.example.usermanagement.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UserLoginRequest {
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+}
