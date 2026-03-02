e6186f00d4cdf7dea94960edab740f21bc6cd65e
cgxspartans <cgxspartans@proton.me>
2026-03-02 09:05:47 -0500

    Add ValidationException for validation module

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.warehouse.employee.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
