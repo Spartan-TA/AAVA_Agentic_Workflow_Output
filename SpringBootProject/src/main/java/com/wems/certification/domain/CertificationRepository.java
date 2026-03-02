4481ac916398344042e466db9e7d79964ffd660f
cgxspartans <cgxspartans@proton.me>
2026-03-02 09:33:49 -0500

    Add CertificationRepository for Certification module

---
+++ SpringBootProject/src/main/java/com/wems/certification/domain/CertificationRepository.java
@@ -0,0 +1,6 @@
+package com.wems.certification.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+}
