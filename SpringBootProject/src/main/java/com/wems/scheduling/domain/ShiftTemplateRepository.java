e99d6136d35e66bff9747835957e1e3f6d95e874
cgxspartans <cgxspartans@proton.me>
2026-03-02 09:29:00 -0500

    Add ShiftTemplateRepository for Scheduling module

---
+++ SpringBootProject/src/main/java/com/wems/scheduling/domain/ShiftTemplateRepository.java
@@ -0,0 +1,6 @@
+package com.wems.scheduling.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShiftTemplateRepository extends JpaRepository<ShiftTemplate, Long> {
+}
