304d91e11e93186732361a27e4952cf99034f711
cgxspartans <cgxspartans@proton.me>
2026-03-02 00:50:15 -0500

    Add AdminUserListResponse DTO

---
+++ SpringBootProject/src/main/java/com/example/usermanagement/dto/AdminUserListResponse.java
@@ -0,0 +1,15 @@
+package com.example.usermanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class AdminUserListResponse {
+    private List<UserProfileResponse> users;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
