852b0dc166c4807663b3578b6e6cd3108550eb49
cgxspartans <cgxspartans@proton.me>
2026-03-02 09:05:29 -0500

    Add LeaveBalanceException for leave module

---
+++ SpringBootProject/src/main/java/com/warehouse/employee/exception/LeaveBalanceException.java
@@ -0,0 +1,7 @@
+package com.warehouse.employee.exception;
+
+public class LeaveBalanceException extends RuntimeException {
+    public LeaveBalanceException(String message) {
+        super(message);
+    }
+}
