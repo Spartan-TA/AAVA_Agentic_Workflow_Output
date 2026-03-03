6b742ad3193ad2e62255fa0a61c6811f5637d290
cgxspartans <cgxspartans@proton.me>
2026-03-03 05:22:56 -0500

    Add ExportStatus enum with values PENDING, IN_PROGRESS, COMPLETED, FAILED

---
+++ SpringBootProject/src/main/java/com/wms/ems/common/enums/ExportStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.common.enums;
+
+/**
+ * Enum representing the status of an export operation.
+ */
+public enum ExportStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    FAILED
+}
