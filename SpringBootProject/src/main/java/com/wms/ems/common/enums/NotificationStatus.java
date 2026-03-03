e48322d44a71fe812d8e87c7288fce484eb7fb74
cgxspartans <cgxspartans@proton.me>
2026-03-03 05:22:53 -0500

    Add NotificationStatus enum with values PENDING, SENT, DELIVERED, FAILED

---
+++ SpringBootProject/src/main/java/com/wms/ems/common/enums/NotificationStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.common.enums;
+
+/**
+ * Enum representing the status of a notification.
+ */
+public enum NotificationStatus {
+    PENDING,
+    SENT,
+    DELIVERED,
+    FAILED
+}
