44d9767c943df1d584feb517c829c6b073147d8e
cgxspartans <cgxspartans@proton.me>
2026-03-03 05:22:31 -0500

    Add EmployeeStatus enum with values ACTIVE, INACTIVE, TERMINATED, ON_LEAVE

---
+++ SpringBootProject/src/main/java/com/wms/ems/common/enums/EmployeeStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.common.enums;
+
+/**
+ * Enum representing the status of an employee.
+ */
+public enum EmployeeStatus {
+    ACTIVE,
+    INACTIVE,
+    TERMINATED,
+    ON_LEAVE
+}
