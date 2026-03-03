380b58f0df050d72e781c30e878a9ea728bb7476
cgxspartans <cgxspartans@proton.me>
2026-03-03 05:22:46 -0500

    Add IncidentStatus enum with values OPEN, INVESTIGATING, RESOLVED, CLOSED

---
+++ SpringBootProject/src/main/java/com/wms/ems/common/enums/IncidentStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.common.enums;
+
+/**
+ * Enum representing the status of an incident.
+ */
+public enum IncidentStatus {
+    OPEN,
+    INVESTIGATING,
+    RESOLVED,
+    CLOSED
+}
