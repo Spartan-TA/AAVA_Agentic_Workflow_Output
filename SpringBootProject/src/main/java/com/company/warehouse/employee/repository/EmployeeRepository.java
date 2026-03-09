507cfb339188a12de71eddef0d32d89d67f3407f
cgxspartans <cgxspartans@proton.me>
2026-03-09 01:59:53 -0400

    Add EmployeeRepository interface for Employee entity

---
+++ SpringBootProject/src/main/java/com/company/warehouse/employee/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.company.warehouse.employee.repository;
+
+import com.company.warehouse.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository for Employee entity.
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeId(String badgeId);
+    boolean existsByBadgeId(String badgeId);
+}
