9334a5812a1d95cdbfd39291967bd99f1d9a895a
cgxspartans <cgxspartans@proton.me>
2026-03-10 14:20:38 -0400

    Add EmployeeRepository with JPA and custom query methods

---
+++ SpringBootProject/employee/src/main/java/com/company/warehouse/employee/repository/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package com.company.warehouse.employee.repository;
+
+import com.company.warehouse.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository for Employee entity.
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+    Page<Employee> findAllByDeletedFalse(Pageable pageable);
+    boolean existsByBadgeIdAndDeletedFalse(String badgeId);
+}
