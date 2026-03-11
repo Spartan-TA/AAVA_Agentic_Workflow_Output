9c8fe68475d28b33e5117aba1721aacb57927add
cgxspartans <cgxspartans@proton.me>
2026-03-11 01:48:13 -0400

    Add LoginDto class

---
+++ SpringBootProject/src/main/java/com/example/app/dto/LoginDto.java
@@ -0,0 +1,12 @@
+package com.example.app.dto;
+
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class LoginDto {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
