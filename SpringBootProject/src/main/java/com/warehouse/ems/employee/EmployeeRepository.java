79075a52e21882e4a4f930bef93aaccc6a53abd1
cgxspartans <cgxspartans@proton.me>
2026-03-11 08:58:03 -0400

    Add EmployeeRepository for Employee CRUD operations

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/employee/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.warehouse.ems.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository for Employee entity.
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeId(String badgeId);
+    boolean existsByBadgeId(String badgeId);
+}
