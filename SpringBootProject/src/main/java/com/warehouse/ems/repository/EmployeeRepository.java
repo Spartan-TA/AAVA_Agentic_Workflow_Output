64d6bc49341392b22a4fc758ce5410bb768e596b
cgxspartans <cgxspartans@proton.me>
2026-03-12 04:48:45 -0400

    Add EmployeeRepository for Employee entity persistence

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.warehouse.ems.repository;
+
+import com.warehouse.ems.domain.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+    Page<Employee> findAllByDeletedFalse(Pageable pageable);
+    boolean existsByBadgeIdAndDeletedFalse(String badgeId);
+}
