e65ae7a5caf5ad11508381c0f32d689c8b4c2359
cgxspartans <cgxspartans@proton.me>
2026-03-17 13:18:32 -0400

    Add EmployeeRepository interface for Employee CRUD

---
+++ SpringBootProject/src/main/java/com/wms/ems/employee/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.wms.ems.employee.repository;
+
+import com.wms.ems.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+/**
+ * Repository for Employee entity CRUD operations.
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+    List<Employee> findAllByDeletedFalse();
+    @Query("SELECT e FROM Employee e WHERE e.deleted = false AND e.department = ?1")
+    List<Employee> findByDepartment(String department);
+}
