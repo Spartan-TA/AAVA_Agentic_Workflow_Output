ddb37d121aea4dbba3d2be48a31ee501a63204b4
cgxspartans <cgxspartans@proton.me>
2026-03-25 08:45:04 -0400

    Add AssetRepository interface extending JpaRepository

---
+++ src/main/java/com/warehouse/ems/repository/AssetRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.ems.repository;
+
+import com.warehouse.ems.entity.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssetRepository extends JpaRepository<Asset, Long> {
+}
