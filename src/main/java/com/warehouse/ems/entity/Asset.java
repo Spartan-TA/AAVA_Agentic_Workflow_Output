249048af6f25bf97f4945878453fbe2fe1b0e40a
cgxspartans <cgxspartans@proton.me>
2026-03-25 08:42:49 -0400

    Add Asset entity with type, serialNumber, and condition

---
+++ src/main/java/com/warehouse/ems/entity/Asset.java
@@ -0,0 +1,24 @@
+package com.warehouse.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "assets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Asset {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String serialNumber;
+
+    private String condition;
+}
