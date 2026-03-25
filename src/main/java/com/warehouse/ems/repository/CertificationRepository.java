b1a7a6f3737937e97e69575dfff763b9467c093e
cgxspartans <cgxspartans@proton.me>
2026-03-25 08:44:52 -0400

    Add CertificationRepository interface extending JpaRepository

---
+++ src/main/java/com/warehouse/ems/repository/CertificationRepository.java
@@ -0,0 +1,9 @@
+package com.warehouse.ems.repository;
+
+import com.warehouse.ems.entity.Certification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+}
