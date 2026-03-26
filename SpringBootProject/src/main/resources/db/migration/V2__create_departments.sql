eeea8c447c5a66de9687390c562f636715b92d3b
cchloet <cchloet@gmail.com>
2026-03-26 10:47:24 -0400

    Removed Demo Files

--- SpringBootProject/src/main/resources/db/migration/V2__create_departments.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE departments (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-CREATE UNIQUE INDEX idx_departments_name ON departments(name);
