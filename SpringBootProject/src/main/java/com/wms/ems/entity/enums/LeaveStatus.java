dce0e05a8db289967c42a265e1a7aba102aa6b65
cgxspartans <cgxspartans@proton.me>
2026-04-14 04:52:27 -0400

    Add LeaveStatus enum (PENDING, APPROVED, DENIED, CANCELLED)

---
+++ SpringBootProject/src/main/java/com/wms/ems/entity/enums/LeaveStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.entity.enums;
+
+/**
+ * Enum representing the status of a leave request.
+ */
+public enum LeaveStatus {
+    PENDING,
+    APPROVED,
+    DENIED,
+    CANCELLED
+}
