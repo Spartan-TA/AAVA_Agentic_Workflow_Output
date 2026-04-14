c53bbb6237e26eb49c7c6acdc3cc7a116dd79051
cgxspartans <cgxspartans@proton.me>
2026-04-14 04:54:01 -0400

    Add GoalStatus enum (NOT_STARTED, IN_PROGRESS, COMPLETED, CANCELLED)

---
+++ SpringBootProject/src/main/java/com/wms/ems/entity/enums/GoalStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.entity.enums;
+
+/**
+ * Enum representing the status of a goal.
+ */
+public enum GoalStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
