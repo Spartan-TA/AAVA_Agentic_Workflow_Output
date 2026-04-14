264fceed95840e3f6dc3cbbe696d521a0199b922
cgxspartans <cgxspartans@proton.me>
2026-04-14 04:53:56 -0400

    Add ReviewStatus enum (DRAFT, SUBMITTED, ACKNOWLEDGED, COMPLETED)

---
+++ SpringBootProject/src/main/java/com/wms/ems/entity/enums/ReviewStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.entity.enums;
+
+/**
+ * Enum representing the status of a performance review.
+ */
+public enum ReviewStatus {
+    DRAFT,
+    SUBMITTED,
+    ACKNOWLEDGED,
+    COMPLETED
+}
