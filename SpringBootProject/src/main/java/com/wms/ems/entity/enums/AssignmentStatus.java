84e471c5440270a24ca0f47150e4b178962a3de8
cgxspartans <cgxspartans@proton.me>
2026-04-14 04:52:51 -0400

    Add AssignmentStatus enum (CHECKED_OUT, RETURNED, OVERDUE)

---
+++ SpringBootProject/src/main/java/com/wms/ems/entity/enums/AssignmentStatus.java
@@ -0,0 +1,10 @@
+package com.wms.ems.entity.enums;
+
+/**
+ * Enum representing the status of an asset assignment.
+ */
+public enum AssignmentStatus {
+    CHECKED_OUT,
+    RETURNED,
+    OVERDUE
+}
