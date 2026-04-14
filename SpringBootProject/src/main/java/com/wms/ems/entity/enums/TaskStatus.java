1b3c25fd73d1de998e9532b854088515b3bb85bb
cgxspartans <cgxspartans@proton.me>
2026-04-14 04:54:15 -0400

    Add TaskStatus enum (PENDING, IN_PROGRESS, COMPLETED, CANCELLED)

---
+++ SpringBootProject/src/main/java/com/wms/ems/entity/enums/TaskStatus.java
@@ -0,0 +1,11 @@
+package com.wms.ems.entity.enums;
+
+/**
+ * Enum representing the status of a task.
+ */
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
