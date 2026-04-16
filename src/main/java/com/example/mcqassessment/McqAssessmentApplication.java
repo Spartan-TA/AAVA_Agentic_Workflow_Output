a62c712b76dcf48b81fdcc06951d201e6091cc84
cgxspartans <cgxspartans@proton.me>
2026-04-16 02:52:09 -0400

    Add main Spring Boot application class

---
+++ src/main/java/com/example/mcqassessment/McqAssessmentApplication.java
@@ -0,0 +1,11 @@
+package com.example.mcqassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqAssessmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(McqAssessmentApplication.class, args);
+    }
+}
