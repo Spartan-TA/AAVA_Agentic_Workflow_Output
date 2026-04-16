10bcdddaaa21fe73cfde3676d006f056d8c6e143
cchloet <cchloet@gmail.com>
2026-04-16 09:35:34 -0400

    Removed Demo Files

--- SpringBootProject/src/main/java/com/example/mcqassessment/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mcqassessment.repository;
-
-import com.example.mcqassessment.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
--- src/main/java/com/example/mcqassessment/repository/AssessmentAttemptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mcqassessment.repository;
-
-import com.example.mcqassessment.domain.AssessmentAttempt;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssessmentAttemptRepository extends JpaRepository<AssessmentAttempt, Long> {
-}
