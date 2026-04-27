b55faff5a9c0c07e4c0814936729f551d8a1ae43
cgxspartans <cgxspartans@proton.me>
2026-04-27 09:18:37 -0400

    Add Role enum for RBAC

---
+++ SpringBootProject/src/main/java/com/company/wms/enums/Role.java
@@ -0,0 +1,11 @@
+package com.company.wms.enums;
+
+/**
+ * Enum representing the roles available in the warehouse management system.
+ */
+public enum Role {
+    ADMIN,
+    MANAGER,
+    SUPERVISOR,
+    EMPLOYEE
+}
