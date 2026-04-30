c5b6b34cc4fb8e3ab48093c7b9f835562e352fbf
cgxspartans <cgxspartans@proton.me>
2026-04-30 04:57:18 -0400

    Add Employee repository interface

---
+++ SpringBootProject/src/main/java/com/warehouse/ems/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.warehouse.ems.repository;
+
+import com.warehouse.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByBadgeIdAndDeletedFalse(String badgeId);
+}
